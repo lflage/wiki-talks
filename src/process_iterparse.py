72feeca35501e0cb4e4f2491b5fac42ad540a109
lflage <flage.lucas@gmail.com>
2024-04-30 00:44:25 +0200

    feat: added parse_page function
    function to parse the wiki page text and organize it as conversation threads. Status: unfinished

--- src/process_iterparse.py
+++ src/process_iterparse.py
@@ -1,5 +1,7 @@
 from lxml import etree
-import bz2, sys, os, json
+import bz2, sys, os, json, re
+import wikitextparser as wtp
+from wikitextparser import remove_markup
 
 # Future CLI settings
 ns_set = {1, 3, 5, 7, 9, 11, 13, 15, 101, 119,711, 829}
@@ -9,9 +11,6 @@ bz2_file = "../dataset/bz2/simplewiki-20240420-pages-meta-current.xml.bz2"
 def process_text(text):
     # Do something with the extracted data
     # TODO: process the text removing html, and unnecessary markdown
-   # print("Title:", title)
-   # print("Text:", text)
-   # print("-----------------------")
     return text
 
 def write_to_jsonl(page_dict, path):
@@ -21,6 +20,24 @@ def write_to_jsonl(page_dict, path):
     else:
         with open(path, "w") as f_out:
             json.dump(page_dict, f_out)
+
+def parse_page(page_dict):
+    page = wtp.parse(page)
+    threads = []
+    for i, section in enumerate(page.sections):
+        section_txt = ""
+        sec_title = section.title
+        if sec_title == None and i == 0:
+            sec_title = page_dict['title']
+
+        sec_text = section.contents
+        section_txt += remove_markup(sec_title)
+        section_txt += "\n" + remove_markup(sec_text)
+        # thread 
+        # if re.match(":", section_txt):
+        
+
+    return list
     
 def parse_wikipedia_dump(xml_file):
     context = etree.iterparse(xml_file, events=("start", "end"))
@@ -50,16 +67,12 @@ def parse_wikipedia_dump(xml_file):
                 text = element.text
             elif current_tag == "page":
                 if cur_dict["ns_value"] is not None and cur_dict["ns_value"] in ns_set:
-                    print("found a Talk namespace")
                     # Process text/ clean the data
-                    cur_dict["text"] = process_text(text)
+                    cur_dict["text"] = text
                     # assert type(text) == list
-                    print(cur_dict["title"])
-                    print("text")
-                    print(cur_dict['ns_value'])
                     # create comment thread hierarchy and write to dict
                     # TODO
-                    # cur_dict["threads"] = parse_threads(text)
+
                     # write current dict as json to jsonl output file
                     write_to_jsonl(cur_dict, out_json)
                     sys.exit()
